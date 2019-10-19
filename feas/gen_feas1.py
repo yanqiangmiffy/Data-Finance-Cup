4cabbc7bc7082eff384925dbe66786ae77333aa6
yanqiangmiffy <1185918903@qq.com>
2019-10-19 22:53:35 +0800

    0.77

---
+++ feas/gen_feas1.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time :    2019/10/19 21:20
+@Author:  yanqiang
+@File: gen_feas1.py
+@Description ：分数0.78108
+"""
+# -*- coding:utf-8 _*-
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import lightgbm as lgb
+import time
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+scaler = StandardScaler()
+
+
+def get_fea(train, test):
+    test['target'] = 'test'
+
+    df = pd.concat((train, test))
+
+    no_features = ['id', 'target', 'isNew', 'x_61', 'x_22', 'x_40', 'x_41', 'x_45', 'x_43', 'begin', 'stop']
+    no_features.extend(['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6', 'x_7', 'x_8', 'x_9', 'x_10', 'x_11', 'x_13',
+                        'x_15', 'x_17', 'x_18', 'x_19', 'x_21', 'x_23', 'x_24', 'x_36', 'x_37', 'x_38', 'x_57', 'x_58',
+                        'x_59', 'x_60', 'x_77', 'x_78', 'x_65', 'x_31', 'x_16', 'x_56', 'x_44', 'x_32', 'x_39'])
+
+    df['begin'] = df['certValidBegin'].apply(lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x)))
+    df['begin_year'] = df['begin'].apply(lambda x: int(x[2:4]))
+    df['begin_month'] = df['begin'].apply(lambda x: int(x[5:7]))
+    df['begin_day'] = df['begin'].apply(lambda x: int(x[8:10]))
+
+    df['certValidStop'] = df['certValidStop'].replace(256000000000, 7858771200)
+    df['stop'] = df['certValidStop'].apply(lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x)))
+    df['stop_year'] = df['stop'].apply(lambda x: int(x[1:4]))
+    df['stop_month'] = df['stop'].apply(lambda x: int(x[5:7]))
+    df['stop_day'] = df['stop'].apply(lambda x: int(x[8:10]))
+
+    for fea in[['bankCard', 'age'], ['job', 'weekday']]:
+        print(fea)
+        i = fea[0]
+        j = fea[1]
+        df['%s--%s' % (i, j)] = df.apply(lambda x: str(x[i]) + str(x[j]), axis=1)
+        lb = LabelEncoder()
+        df['%s--%s' % (i, j)] = lb.fit_transform(df['%s--%s' % (i, j)])
+
+    train = df[df['target'] != 'test']
+    test = df[df['target'] == 'test']
+
+    train = train[train['target'].notnull()].reset_index(drop=True)
+    print(train.shape)
+    return train, test, no_features
+
+label = pd.read_csv('../data/train_target.csv')
+train = pd.read_csv('../data/train.csv')
+train = pd.merge(train, label, on='id', how='left')
+test = pd.read_csv('../data/test.csv')
+
+train, test, no_features = get_fea(train, test)
+print('get_fea ok')
+
+label = train['target']
+label = label.values.astype(int)
+
+sub = test[['id']]
+
+features = [fea for fea in train.columns if fea not in no_features]
+train_df = train[features]
+test_df = test[features]
+
+scaler.fit(train_df)
+train_df = scaler.transform(train_df)
+test_df = scaler.transform(test_df)
+
+params = {
+    'task': 'train',
+    'boosting_type': 'gbdt',
+    'max_depth': 4,
+    'objective': 'binary',
+    'metric': {'auc'},
+    'num_leaves': 16,
+    'learning_rate': 0.01,
+    'feature_fraction': 0.8,
+    'bagging_fraction': 0.7,
+    'random_state': 1024,
+    'n_jobs': -1,
+}
+
+trn_data = lgb.Dataset(train_df, label)
+val_data = lgb.Dataset(train_df, label)
+num_round = 550
+model = lgb.train(params,
+                  trn_data,
+                  num_round,
+                  valid_sets=[trn_data, val_data],
+                  verbose_eval=10,
+                  early_stopping_rounds=100,
+                  feature_name=features)
+
+r = model.predict(test_df, num_iteration=model.best_iteration)
+
+test['target'] = r
+
+test[['id', 'target']].to_csv('../result/submission_lgb_all.csv', index=None)
+
+# 0.823104
+# 0.829545
+
+
--- gen_feas.py
+++ gen_feas.py
@@ -2,63 +2,18 @@ import pandas as pd
 from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-
-
-def add_noise(series, noise_level):
-    return series * (1 + noise_level * np.random.randn(len(series)))
-
-
-def target_encode(trn_series=None,
-                  tst_series=None,
-                  target=None,
-                  min_samples_leaf=1,
-                  smoothing=1,
-                  noise_level=0):
-    """
-    Smoothing is computed like in the following paper by Daniele Micci-Barreca
-    https://kaggle2.blob.core.windows.net/forum-message-attachments/225952/7441/high%20cardinality%20categoricals.pdf
-    trn_series : training categorical feature as a pd.Series
-    tst_series : test categorical feature as a pd.Series
-    target : target data as a pd.Series
-    min_samples_leaf (int) : minimum samples to take category average into account
-    smoothing (int) : smoothing effect to balance categorical average vs prior
-    """
-    assert len(trn_series) == len(target)
-    assert trn_series.name == tst_series.name
-    temp = pd.concat([trn_series, target], axis=1)
-    # Compute target mean
-    averages = temp.groupby(by=trn_series.name)[target.name].agg(["mean", "count"])
-    # Compute smoothing
-    smoothing = 1 / (1 + np.exp(-(averages["count"] - min_samples_leaf) / smoothing))
-    # Apply average function to all target data
-    prior = target.mean()
-    # The bigger the count the less full_avg is taken into account
-    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
-    averages.drop(["mean", "count"], axis=1, inplace=True)
-    # Apply averages to trn and tst series
-    ft_trn_series = pd.merge(
-        trn_series.to_frame(trn_series.name),
-        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
-        on=trn_series.name,
-        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
-    # pd.merge does not keep the index so restore it
-    ft_trn_series.index = trn_series.index
-    ft_tst_series = pd.merge(
-        tst_series.to_frame(tst_series.name),
-        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
-        on=tst_series.name,
-        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
-    # pd.merge does not keep the index so restore it
-    ft_tst_series.index = tst_series.index
-    return add_noise(ft_trn_series, noise_level), add_noise(ft_tst_series, noise_level)
-
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from utils import *
+from itertools import combinations
 
 train = pd.read_csv("new_data/train.csv")
 train_target = pd.read_csv('new_data/train_target.csv')
 train = train.merge(train_target, on='id')
 test = pd.read_csv("new_data/test.csv")
+test['target'] = [0] * len(test)
 df = pd.concat([train, test], sort=False, axis=0)
+
+# =========  简单数据描述 ============
 stats = []
 for col in df.columns:
     stats.append((col, train[col].nunique(), train[col].isnull().sum() * 100 / train.shape[0],
@@ -69,9 +24,9 @@ stats_df = pd.DataFrame(stats, columns=['Feature', 'Unique_values', 'Percentage 
 stats_df.sort_values('Unique_values', ascending=False, inplace=True)
 stats_df.to_excel('tmp/stats_df.xlsx', index=None)
 
-# 特征工程
+# ========= 数据预处理 ============
 df.fillna(value=-999, inplace=True)  # bankCard存在空值
-# 删除重复列
+
 duplicated_features = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                        'x_7', 'x_8', 'x_9', 'x_10', 'x_11', 'x_13',
                        'x_15', 'x_17', 'x_18', 'x_19', 'x_21',
@@ -82,90 +37,94 @@ duplicated_features = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                       ['x_43'] + \
                       ['x_45'] + \
                       ['x_61']
-df = df.drop(columns=duplicated_features)
-print(df.shape)
+df = df.drop(columns=duplicated_features)  # 删除重复列
 
-no_features = ['id', 'target'] + ['bankCard', 'residentAddr', 'certId', 'dist']
-features = []
-numerical_features = ['lmt', 'certValidBegin', 'certValidStop']
-categorical_features = [fea for fea in df.columns if fea not in numerical_features + no_features]
+# ================ 特征工程 ==========
 
-# 数值特征处理
+no_features = ['id', 'target'] + ['bankCard', 'residentAddr', 'certId', 'dist']  # 需要删除的特征
+numerical_features = ['lmt', 'certValidBegin', 'certValidStop']  # 类别特征
+
+# ------ 数值特征 ------
+
 df['certValidPeriod'] = df['certValidStop'] - df['certValidBegin']
-for feat in numerical_features + ['certValidPeriod']:
-    df[feat] = df[feat].rank() / float(df.shape[0])  # 排序，并且进行归一化
-# 类别特征处理
+numerical_features.append('certValidPeriod')
 
-# 特殊处理
-# bankCard 5991
-# residentAddr 5288
-# certId 4033
-# dist 3738
-cols = ['bankCard', 'residentAddr', 'certId', 'dist']
 # 计数
+cols = ['bankCard', 'residentAddr', 'certId', 'dist',
+        'lmt', 'ethnic', 'age', 'setupHour', 'job',
+        'linkRela', 'highestEdu', 'edu', 'x_33', 'weekday',
+        'x_34', 'basicLevel']
 for col in cols:
     col_nums = dict(df[col].value_counts())
-    df[col + '_nums'] = df[col].apply(lambda x: col_nums[x])
+    df[col + '_cnt'] = df[col].apply(lambda x: col_nums[x])
+    numerical_features.append(col + '_cnt')
+
 # 对lmt进行mean encoding
 for fea in tqdm(cols):
-    grouped_df = df.groupby(fea).agg({'lmt': ['min', 'max', 'mean', 'sum', 'median']})
+    grouped_df = df.groupby(fea).agg(
+        {'lmt': ['min', 'max', 'mean', 'sum', 'median', pd.DataFrame.skew, pd.DataFrame.kurtosis]})
     grouped_df.columns = [fea + '_' + '_'.join(col).strip() for col in grouped_df.columns.values]
+
+    numerical_features.extend(list(grouped_df.columns))
+
     grouped_df = grouped_df.reset_index()
     # print(grouped_df)
     df = pd.merge(df, grouped_df, on=fea, how='left')
-df = df.drop(columns=cols)  # 删除四列
 
-import time
+scale = MinMaxScaler()
+for feat in numerical_features:
+    # df[feat] = df[feat].rank() / float(df.shape[0])  # 排序，并且进行归一化
+    df[feat] = scale.fit_transform(df[feat].values.reshape(-1, 1))  # 排序，并且进行归一化
 
-combs = [
-    ('ethnic', 'highestEdu'),
-    ('basicLevel', 'linkRela'),
-]
+for n_c, (f1, f2) in tqdm(enumerate(combinations(cols, 2))):
+    name1 = f1 + "_plus_" + f2
+    df[name1 + '_add'] = df[f1] + df[f2]
+    df[name1 + '_diff'] = df[f1] - df[f2]
+    df[name1 + '_multiply'] = df[f1] * df[f2]
+    df[name1 + '_division'] = df[f1] / (df[f2] + 1)
+    numerical_features.append(name1 + '_add')
+    numerical_features.append(name1 + '_diff')
+    numerical_features.append(name1 + '_multiply')
+    numerical_features.append(name1 + '_division')
+# lmt cut
+df['lmt_bin'] = pd.cut(df['lmt'], 7, labels=range(7)).astype(int)
 
-col_vals_dict = {c: list(train[c].unique()) for c in categorical_features}
-print(col_vals_dict)
-embed_cols = []
+print(df['lmt_bin'])
+# ------- 类别特征 -------
+categorical_features = [fea for fea in df.columns if fea not in numerical_features + no_features]
+
+# 组合特征
+col_vals_dict = {c: list(df[c].unique()) for c in categorical_features}
+embed_cols = ['lmt']
 for c in col_vals_dict:
-    if 6<=len(col_vals_dict[c]) <=50:
+    if 6 <= len(col_vals_dict[c]) <= 50:
         embed_cols.append(c)
-        print(c + ': %d values' % len(col_vals_dict[c]))  # look at value counts to know the embedding dimensions
-print('\n')
-print(len(embed_cols))
-start = time.time()
-from itertools import combinations
-
-# for n_c, (f1, f2) in enumerate(combs):
 for n_c, (f1, f2) in tqdm(enumerate(combinations(embed_cols, 2))):
-    print(f1, f2)
     # f1 = rv[0]
     # f2 = rv[1]
     name1 = f1 + "_plus_" + f2
-    print('current feature %60s %4d in %5.1f'
-          % (name1, n_c + 1, (time.time() - start) / 60), end='')
-    print('\r' * 75, end='')
     df[name1] = df[f1].apply(lambda x: str(x)) + "_" + df[f2].apply(lambda x: str(x))
     # Label Encode
     lbl = LabelEncoder()
     lbl.fit(list(df[name1].values))
     df[name1] = lbl.transform(list(df[name1].values))
-
     categorical_features.append(name1)
 
-# dummies
-# df = pd.get_dummies(df, columns=categorical_features)
-# df.to_csv('tmp/df.csv', index=None)
-print("df.shape:", df.shape)
+# # dummies
+# # df = pd.get_dummies(df, columns=categorical_features)
 
-for f in categorical_features:
-    df[f + "_avg"], df[f + "_avg"] = target_encode(trn_series=df[f],
-                                                   tst_series=df[f],
-                                                   target=df['target'],
-                                                   min_samples_leaf=200,
-                                                   smoothing=10,
-                                                   noise_level=0)
+#
+# for f in cols:
+#     df[f + "_avg"], df[f + "_avg"] = target_encode(trn_series=df[f],
+#                                                    tst_series=df[f],
+#                                                    target=df['target'],
+#                                                    min_samples_leaf=200,
+#                                                    smoothing=10,
+#                                                    noise_level=0)
 # df = pd.get_dummies(df, columns=categorical_features)
 train, test = df[:len(train)], df[len(train):]
-
+df.head().to_csv('tmp/df.csv', index=None)
+print("df.shape:", df.shape)
 features = [fea for fea in df.columns if fea not in no_features]
 
 
