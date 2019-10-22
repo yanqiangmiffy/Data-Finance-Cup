d37d1d5e8ef8153414f791d5b67f80e74ea4211d
yanqiangmiffy <1185918903@qq.com>
2019-10-23 00:13:29 +0800

    0.78478

---
+++ 11_params_opt.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time :    2019/10/22 10:35
+@Author:  yanqiang
+@File: 11_params_opt.py
+参数优化
+"""
+
+# Importing core libraries
+import numpy as np
+import pandas as pd
+from time import time
+import pprint
+import joblib
+
+# Suppressing warnings because of skopt verbosity
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# Our example dataset
+from sklearn.datasets import load_boston
+
+# Classifiers
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+import lightgbm as lgb
+import xgboost as xgb
+from catboost import CatBoostClassifier
+
+# Hyperparameters distributions
+from scipy.stats import randint
+from scipy.stats import uniform
+
+# Model selection
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import cross_val_score
+
+# Metrics
+from sklearn.metrics import average_precision_score, roc_auc_score, mean_absolute_error
+from sklearn.metrics import make_scorer
+
+# Skopt functions
+from skopt import BayesSearchCV
+from skopt import gp_minimize  # Bayesian optimization using Gaussian Processes
+from skopt.space import Real, Categorical, Integer
+from skopt.utils import use_named_args  # decorator to convert a list of parameters to named arguments
+from skopt.callbacks import DeadlineStopper  # Stop the optimization before running out of a fixed budget of time.
+from skopt.callbacks import VerboseCallback  # Callback to control the verbosity
+# Stop the optimization If the last two positions at which the objective has been evaluated are less than delta
+from skopt.callbacks import DeltaXStopper
+
+# Uploading the Boston dataset
+X, y = load_boston(return_X_y=True)
+# Transforming the problem into a classification (unbalanced)
+y_bin = (y > np.percentile(y, 90)).astype(int)
+clf = lgb.LGBMClassifier(boosting_type='gbdt',
+                         class_weight='balanced',
+                         objective='binary',
+                         n_jobs=1,
+                         verbose=0)
+search_spaces = {
+    'learning_rate': Real(0.01, 1.0, 'log-uniform'),
+    'num_leaves': Integer(2, 500),
+    'max_depth': Integer(0, 500),
+    'min_child_samples': Integer(0, 200),
+    'max_bin': Integer(100, 100000),
+    'subsample': Real(0.01, 1.0, 'uniform'),
+    'subsample_freq': Integer(0, 10),
+    'colsample_bytree': Real(0.01, 1.0, 'uniform'),
+    'min_child_weight': Integer(0, 10),
+    'subsample_for_bin': Integer(100000, 500000),
+    'reg_lambda': Real(1e-9, 1000, 'log-uniform'),
+    'reg_alpha': Real(1e-9, 1.0, 'log-uniform'),
+    'scale_pos_weight': Real(1e-6, 500, 'log-uniform'),
+    'n_estimators': Integer(10, 10000)
+}
+
+
+# Reporting util for different optimizers
+def report_perf(optimizer, X, y, title, callbacks=None):
+    """
+    A wrapper for measuring time and performances of different optmizers
+
+    optimizer = a sklearn or a skopt optimizer
+    X = the training set
+    y = our target
+    title = a string label for the experiment
+    """
+    start = time()
+    if callbacks:
+        optimizer.fit(X, y, callback=callbacks)
+    else:
+        optimizer.fit(X, y)
+    best_score = optimizer.best_score_
+    best_score_std = optimizer.cv_results_['std_test_score'][optimizer.best_index_]
+    best_params = optimizer.best_params_
+    print((title + " took %.2f seconds,  candidates checked: %d, best CV score: %.3f "
+           + u"\u00B1" + " %.3f") % (time() - start,
+                                     len(optimizer.cv_results_['params']),
+                                     best_score,
+                                     best_score_std))
+    print('Best parameters:')
+    pprint.pprint(best_params)
+    print()
+    return best_params
+
+
+# Converting average precision score into a scorer suitable for model selection
+avg_prec = make_scorer(average_precision_score, greater_is_better=True, needs_proba=True)
+# Setting a 5-fold stratified cross-validation (note: shuffle=True)
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+opt = BayesSearchCV(clf,
+                    search_spaces,
+                    scoring=avg_prec,
+                    cv=skf,
+                    n_iter=40,
+                    n_jobs=-1,
+                    refit=True,
+                    optimizer_kwargs={'base_estimator': 'GP'},
+                    random_state=22,
+                    return_train_score=False,
+                    )
+
+best_params = report_perf(opt, X, y_bin, 'LightGBM',
+                          callbacks=[DeltaXStopper(0.001),
+                                     DeadlineStopper(60 * 5)])
--- gen_feas.py
+++ gen_feas.py
@@ -1,19 +1,12 @@
 import pandas as pd
 from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
-import numpy as np
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from utils import *
-from itertools import combinations
 
 train = pd.read_csv("new_data/train.csv")
 train_target = pd.read_csv('new_data/train_target.csv')
 train = train.merge(train_target, on='id')
 test = pd.read_csv("new_data/test.csv")
-test['target'] = [0] * len(test)
 df = pd.concat([train, test], sort=False, axis=0)
-
-# =========  简单数据描述 ============
 stats = []
 for col in df.columns:
     stats.append((col, train[col].nunique(), train[col].isnull().sum() * 100 / train.shape[0],
@@ -24,9 +17,9 @@ stats_df = pd.DataFrame(stats, columns=['Feature', 'Unique_values', 'Percentage 
 stats_df.sort_values('Unique_values', ascending=False, inplace=True)
 stats_df.to_excel('tmp/stats_df.xlsx', index=None)
 
-# ========= 数据预处理 ============
-df.fillna(value=-999, inplace=True)  # bankCard存在空值
-
+# 特征工程
+df.fillna(value=0, inplace=True)  # bankCard存在空值
+# 删除重复列
 duplicated_features = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                        'x_7', 'x_8', 'x_9', 'x_10', 'x_11', 'x_13',
                        'x_15', 'x_17', 'x_18', 'x_19', 'x_21',
@@ -37,95 +30,46 @@ duplicated_features = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                       ['x_43'] + \
                       ['x_45'] + \
                       ['x_61']
-df = df.drop(columns=duplicated_features)  # 删除重复列
+df = df.drop(columns=duplicated_features)
+print(df.shape)
 
-# ================ 特征工程 ==========
+no_features = ['id', 'target'] + ['bankCard', 'residentAddr', 'certId', 'dist']
+features = []
+numerical_features = ['lmt', 'certValidBegin', 'certValidStop']
+categorical_features = [fea for fea in df.columns if fea not in numerical_features + no_features]
 
-no_features = ['id', 'target'] + ['bankCard', 'residentAddr', 'certId', 'dist']  # 需要删除的特征
-numerical_features = ['lmt', 'certValidBegin', 'certValidStop']  # 类别特征
-
-# ------ 数值特征 ------
-
+# 数值特征处理
 df['certValidPeriod'] = df['certValidStop'] - df['certValidBegin']
-numerical_features.append('certValidPeriod')
+for feat in numerical_features + ['certValidPeriod']:
+    df[feat] = df[feat].rank() / float(df.shape[0])  # 排序，并且进行归一化
+# 类别特征处理
 
+# 特殊处理
+# bankCard 5991
+# residentAddr 5288
+# certId 4033
+# dist 3738
+cols = ['bankCard', 'residentAddr', 'certId', 'dist']
 # 计数
-cols = ['bankCard', 'residentAddr', 'certId', 'dist',
-        'lmt', 'ethnic', 'age', 'setupHour', 'job',
-        'linkRela', 'highestEdu', 'edu', 'x_33', 'weekday',
-        'x_34', 'basicLevel']
 for col in cols:
     col_nums = dict(df[col].value_counts())
-    df[col + '_cnt'] = df[col].apply(lambda x: col_nums[x])
-    numerical_features.append(col + '_cnt')
-
+    df[col + '_nums'] = df[col].apply(lambda x: col_nums[x])
 # 对lmt进行mean encoding
 for fea in tqdm(cols):
-    grouped_df = df.groupby(fea).agg(
-        {'lmt': ['min', 'max', 'mean', 'sum', 'median', pd.DataFrame.skew, pd.DataFrame.kurtosis]})
+    grouped_df = df.groupby(fea).agg({'lmt': ['min', 'max', 'mean', 'sum', 'median']})
     grouped_df.columns = [fea + '_' + '_'.join(col).strip() for col in grouped_df.columns.values]
-
-    numerical_features.extend(list(grouped_df.columns))
-
     grouped_df = grouped_df.reset_index()
     # print(grouped_df)
     df = pd.merge(df, grouped_df, on=fea, how='left')
+df = df.drop(columns=cols)  # 删除四列
 
-scale = MinMaxScaler()
-for feat in numerical_features:
-    # df[feat] = df[feat].rank() / float(df.shape[0])  # 排序，并且进行归一化
-    df[feat] = scale.fit_transform(df[feat].values.reshape(-1, 1))  # 排序，并且进行归一化
-
-for n_c, (f1, f2) in tqdm(enumerate(combinations(cols, 2))):
-    name1 = f1 + "_plus_" + f2
-    df[name1 + '_add'] = df[f1] + df[f2]
-    df[name1 + '_diff'] = df[f1] - df[f2]
-    df[name1 + '_multiply'] = df[f1] * df[f2]
-    df[name1 + '_division'] = df[f1] / (df[f2] + 1)
-    numerical_features.append(name1 + '_add')
-    numerical_features.append(name1 + '_diff')
-    numerical_features.append(name1 + '_multiply')
-    numerical_features.append(name1 + '_division')
-# lmt cut
-df['lmt_bin'] = pd.cut(df['lmt'], 7, labels=range(7)).astype(int)
-
-print(df['lmt_bin'])
-# ------- 类别特征 -------
-categorical_features = [fea for fea in df.columns if fea not in numerical_features + no_features]
-
-# 组合特征
-col_vals_dict = {c: list(df[c].unique()) for c in categorical_features}
-embed_cols = ['lmt']
-for c in col_vals_dict:
-    if 6 <= len(col_vals_dict[c]) <= 50:
-        embed_cols.append(c)
-for n_c, (f1, f2) in tqdm(enumerate(combinations(embed_cols, 2))):
-    # f1 = rv[0]
-    # f2 = rv[1]
-    name1 = f1 + "_plus_" + f2
-    df[name1] = df[f1].apply(lambda x: str(x)) + "_" + df[f2].apply(lambda x: str(x))
-    # Label Encode
-    lbl = LabelEncoder()
-    lbl.fit(list(df[name1].values))
-    df[name1] = lbl.transform(list(df[name1].values))
-    categorical_features.append(name1)
-
-# # dummies
-# # df = pd.get_dummies(df, columns=categorical_features)
-
-#
-# for f in cols:
-#     df[f + "_avg"], df[f + "_avg"] = target_encode(trn_series=df[f],
-#                                                    tst_series=df[f],
-#                                                    target=df['target'],
-#                                                    min_samples_leaf=200,
-#                                                    smoothing=10,
-#                                                    noise_level=0)
-# df = pd.get_dummies(df, columns=categorical_features)
-train, test = df[:len(train)], df[len(train):]
-df.head().to_csv('tmp/df.csv', index=None)
+# dummies
+df = pd.get_dummies(df, columns=categorical_features)
+df.to_csv('tmp/df.csv', index=None)
 print("df.shape:", df.shape)
+
 features = [fea for fea in df.columns if fea not in no_features]
+train, test = df[:len(train)], df[len(train):]
 
 
 def load_data():
