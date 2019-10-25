234d00fd4a4528371d614443f130b71774dfbbae
yanqiangmiffy <1185918903@qq.com>
2019-10-26 01:03:31 +0800

    0.78676

--- feas/gen_feas2.py
+++ feas/gen_feas2.py
@@ -7,22 +7,10 @@ train_target = pd.read_csv('new_data/train_target.csv')
 train = train.merge(train_target, on='id')
 test = pd.read_csv("new_data/test.csv")
 
-train['missing'] = (train == -999).sum(axis=1).astype(float)
-test['missing'] = (test == -999).sum(axis=1).astype(float)
-
 df = pd.concat([train, test], sort=False, axis=0)
-stats = []
-for col in df.columns:
-    stats.append((col, train[col].nunique(), train[col].isnull().sum() * 100 / train.shape[0],
-                  train[col].value_counts(normalize=True, dropna=False).values[0] * 100, train[col].dtype))
-
-stats_df = pd.DataFrame(stats, columns=['Feature', 'Unique_values', 'Percentage of missing values',
-                                        'Percentage of values in the biggest category', 'type'])
-stats_df.sort_values('Unique_values', ascending=False, inplace=True)
-stats_df.to_excel('tmp/stats_df.xlsx', index=None)
-
 # 特征工程
-df.fillna(value=0, inplace=True)  # bankCard存在空值
+df['missing'] = (df == -1).sum(axis=1).astype(float)  # 统计每行中为-999的个数
+df['bankCard'] = df['bankCard'].fillna(value=0)  # bankCard存在空值
 # 删除重复列
 duplicated_features = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                        'x_7', 'x_8', 'x_9', 'x_10', 'x_11', 'x_13',
@@ -34,12 +22,25 @@ duplicated_features = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                       ['x_43'] + \
                       ['x_45'] + \
                       ['x_61']
+# 类别组合特征
+count = 0
+for c in duplicated_features:
+    if count == 0:
+        df['new_ind' + str(-1)] = df[c].astype(str) + '_'
+        count += 1
+    else:
+        df['new_ind' + str(-1)] += df[c].astype(str) + '_'
+for c in ['new_ind' + str(-1)]:
+    d = df[c].value_counts().to_dict()
+    df['%s_count' % c] = df[c].apply(lambda x: d.get(x, 0))
+df.drop(columns=['new_ind' + str(-1)], inplace=True)
+
 df = df.drop(columns=duplicated_features)
 print(df.shape)
 
 no_features = ['id', 'target'] + ['bankCard', 'residentAddr', 'certId', 'dist', 'new_ind1', 'new_ind2']
 features = []
-numerical_features = ['lmt', 'certValidBegin', 'certValidStop', 'missing']
+numerical_features = ['lmt', 'certValidBegin', 'certValidStop', 'missing']  # 不是严格意义的数值特征，可以当做类别特征
 categorical_features = [fea for fea in df.columns if fea not in numerical_features + no_features]
 
 group_features1 = [c for c in categorical_features if 'x_' in c]  # 匿名
@@ -48,7 +49,6 @@ group_features3 = ['lmt', 'certValidBegin', 'certValidStop']  # 征信1
 
 group_features4 = ['age', 'job', 'ethnic', 'basicLevel', 'linkRela']  # 基本属性
 group_features5 = ['ncloseCreditCard', 'unpayIndvLoan', 'unpayOtherLoan', 'unpayNormalLoan', '5yearBadloan']
-group_features6 = ['ncloseCreditCard', 'unpayIndvLoan', 'unpayOtherLoan', 'unpayNormalLoan', '5yearBadloan']
 # 重要特征+其他组合
 # group_features5 = group_features1 + ['lmt']
 # group_features6 = group_features1 + ['certValidBegin']
@@ -86,8 +86,6 @@ for index, ind_features in enumerate(group_features):
 
 # 数值特征处理
 df['certValidPeriod'] = df['certValidStop'] - df['certValidBegin']
-for feat in numerical_features + ['certValidPeriod']:
-    df[feat] = df[feat].rank() / float(df.shape[0])  # 排序，并且进行归一化
 # 类别特征处理
 
 # 特殊处理
@@ -98,20 +96,25 @@ for feat in numerical_features + ['certValidPeriod']:
 cols = ['bankCard', 'residentAddr', 'certId', 'dist']
 # 计数
 for col in cols:
-    col_nums = dict(df[col].value_counts())
-    df[col + '_nums'] = df[col].apply(lambda x: col_nums[x])
+    df['{}_count'.format(col)] = df.groupby(col)['id'].transform('count')
+
 # 对lmt进行mean encoding
 for fea in tqdm(cols):
-    grouped_df = df.groupby(fea).agg({'lmt': [ 'mean', 'median']})
+    grouped_df = df.groupby(fea).agg({'lmt': ['mean', 'median']})
     grouped_df.columns = [fea + '_' + '_'.join(col).strip() for col in grouped_df.columns.values]
     grouped_df = grouped_df.reset_index()
     # print(grouped_df)
     df = pd.merge(df, grouped_df, on=fea, how='left')
 df = df.drop(columns=cols)  # 删除四列
 
+# cols = ['certValidPeriod', 'age', 'job', 'ethnic', 'basicLevel',
+#         'linkRela']
+# for col in cols:
+#     df['{}_count'.format(col)] = df.groupby(col)['id'].transform('count')
+
 # dummies
 df = pd.get_dummies(df, columns=categorical_features)
-df.to_csv('tmp/df.csv', index=None)
+df.head().to_csv('tmp/df.csv', index=None)
 print("df.shape:", df.shape)
 
 features = [fea for fea in df.columns if fea not in no_features]
