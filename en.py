fe2bb0977e41f48b3cc2c00b6c704dda82bb2aed
yanqiangmiffy <1185918903@qq.com>
2019-11-07 16:11:32 +0800

    融合 提高

--- en.py
+++ en.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 lgb = pd.read_csv('result/0.7087926129573728.csv')
 xgb = pd.read_csv('result/xgb.csv')
-cnn = pd.read_csv('result/NN_EntityEmbed_10fold-sub.csv')
+cnn = pd.read_csv('result/02_cnn10.6907014032940847_cnn.csv')
 
 res = lgb.copy()
 res['target'] = cnn['target'] * 0.1 + xgb['target'] * 0.9
