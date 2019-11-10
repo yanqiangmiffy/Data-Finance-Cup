37f538bb663e37db9c3fb8adde4c4aab6f50b74f
yanqiangmiffy <1185918903@qq.com>
2019-11-10 22:16:00 +0800

    bayesopt 0.790

--- 18_lgb_bayesopt.py
+++ 18_lgb_bayesopt.py
@@ -7,6 +7,7 @@
 @time: 2019-11-09 23:16
 @description:https://www.kaggle.com/qwe1398775315/eda-lgbm-bayesianoptimization/comments#397789
 """
+from sklearn.model_selection import train_test_split
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 from pandas.io.json import json_normalize
@@ -30,8 +31,9 @@ X_test=test[features].values
 
 def lgb_eval(num_leaves, max_depth, lambda_l2, lambda_l1, min_child_samples, bagging_fraction, feature_fraction):
     params = {
-        "objective": "regression",
-        "metric": "rmse",
+        'boosting_type': 'gbdt',
+        'objective': 'binary',
+        "metric": "auc",
         "num_leaves": int(num_leaves),
         "max_depth": int(max_depth),
         "lambda_l2": lambda_l2,
@@ -54,11 +56,11 @@ def lgb_eval(num_leaves, max_depth, lambda_l2, lambda_l1, min_child_samples, bag
                        early_stopping_rounds=100,
                        stratified=False,
                        nfold=5)
-    return -cv_result['rmse-mean'][-1]
+    return cv_result['auc-mean'][-1]
 
 def param_tuning(init_points,num_iter,**args):
-    lgbBO = BayesianOptimization(lgb_eval, {'num_leaves': (25, 50),
-                                                'max_depth': (5, 15),
+    lgbBO = BayesianOptimization(lgb_eval, {'num_leaves': (16, 50),
+                                                'max_depth': (3, 15),
                                                 'lambda_l2': (0.0, 0.05),
                                                 'lambda_l1': (0.0, 0.05),
                                                 'bagging_fraction': (0.5, 0.8),
@@ -70,4 +72,42 @@ def param_tuning(init_points,num_iter,**args):
     return lgbBO
 
 
-result = param_tuning(5,15)
+result = param_tuning(15,20)
+print(result.max['params'])
+
+
+def lgb_train(num_leaves, max_depth, lambda_l2, lambda_l1, min_child_samples, bagging_fraction, feature_fraction):
+    params = {
+        'boosting_type': 'gbdt',
+        'objective': 'binary',
+        "metric": "auc",
+        "num_leaves": int(num_leaves),
+        "max_depth": int(max_depth),
+        "lambda_l2": lambda_l2,
+        "lambda_l1": lambda_l1,
+        "num_threads": 4,
+        "min_child_samples": int(min_child_samples),
+        "learning_rate": 0.01,
+        "bagging_fraction": bagging_fraction,
+        "feature_fraction": feature_fraction,
+        "subsample_freq": 5,
+        "bagging_seed": 42,
+        "verbosity": -1
+    }
+    t_x, v_x, t_y, v_y = train_test_split(X_train, y_train, test_size=0.2)
+    lgtrain = lgb.Dataset(t_x, label=t_y)
+    lgvalid = lgb.Dataset(v_x, label=v_y)
+    model = lgb.train(params, lgtrain, 2000, valid_sets=[lgvalid], early_stopping_rounds=100, verbose_eval=100)
+    pred_test_y = model.predict(X_test, num_iteration=model.best_iteration)
+    return pred_test_y, model
+
+prediction1,model1 = lgb_train(**result.max['params'])
+prediction2,model2 = lgb_train(**result.max['params'])
+prediction3,model3 = lgb_train(**result.max['params'])
+
+from pandas import DataFrame
+result = DataFrame()
+result['id'] = test['id']
+result['target'] = (prediction1+prediction2+prediction3)/3
+result.to_csv('result/bayes_opt.csv', index=False, sep=",",float_format='%.8f')
+
--- en.py
+++ en.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 lgb = pd.read_csv('result/0.7087926129573728.csv')
 xgb = pd.read_csv('result/xgb.csv')
-cnn = pd.read_csv('result/02_cnn10.6907014032940847_cnn.csv')
+cnn = pd.read_csv('result/stacking.csv')
 
 res = lgb.copy()
-res['target'] = cnn['target'] * 0.1 + xgb['target'] * 0.9
+res['target'] = cnn['target'] * 0.4 + xgb['target'] * 0.6
 # res=lgb*0.8+xgb*0.2   # 0.877785  xgb 原先
 res.to_csv('result/en73.csv', index=None,float_format='%.6f')
