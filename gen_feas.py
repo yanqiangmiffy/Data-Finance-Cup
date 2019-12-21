55efd7c6438c697bb4d1ddcd30b12c2c6e9fe0e0
yanqiangmiffy <1185918903@qq.com>
2019-12-21 10:00:08 +0800

    贪心开源

--- gen_feas.py
+++ gen_feas.py
@@ -61,6 +61,7 @@ duplicated_features = ['x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6',
                       ['x_43'] + \
                       ['x_45'] + \
                       ['x_61']
+# 121021
 # 类别组合特征
 count = 0
 for c in duplicated_features:
@@ -74,20 +75,21 @@ for c in ['new_ind' + str(-1)]:
     df['%s_count' % c] = df[c].apply(lambda x: d.get(x, 0))
 df.drop(columns=['new_ind' + str(-1)], inplace=True)
 df = df.drop(columns=duplicated_features)
+
 print(df.shape)
 simple_statics()
 
 no_features = ['id', 'target'] + ['bankCard', 'residentAddr', 'certId', 'dist', 'new_ind1', 'new_ind2']
+
 features = []
 numerical_features = ['lmt', 'certValidBegin', 'certValidStop', 'missing']  # 不是严格意义的数值特征，可以当做类别特征
 categorical_features = [fea for fea in df.columns if fea not in numerical_features + no_features]
-
+# 1、构造分组组合特征和count特征
 group_features1 = [c for c in categorical_features if 'x_' in c]  # 匿名
 group_features2 = ['bankCard', 'residentAddr', 'certId', 'dist']  # 地区特征
 group_features3 = ['lmt', 'certValidBegin', 'certValidStop']  # 征信1
-
 group_features4 = ['age', 'job', 'ethnic', 'basicLevel', 'linkRela']  # 基本属性
-group_features5 = ['ncloseCreditCard', 'unpayIndvLoan', 'unpayOtherLoan', 'unpayNormalLoan', '5yearBadloan']
+group_features5 = ['ncloseCreditCard', 'unpayIndvLoan', 'unpayOtherLoan', 'unpayNormalLoan', '5yearBadloan'] # 贷款
 
 group_features = [
     group_features1, group_features2, group_features3, group_features4,
@@ -112,6 +114,13 @@ from sklearn.preprocessing import LabelEncoder
 
 
 def create_group_fea(df_, groups_fea, group_name):
+    """
+    类别组合特征
+    :param df_:
+    :param groups_fea:
+    :param group_name:
+    :return:
+    """
     count = 0
     for c in groups_fea:
         if count == 0:
@@ -127,13 +136,13 @@ def create_group_fea(df_, groups_fea, group_name):
     # df_.drop(columns=[group_name], inplace=True)
     return df_
 
-
+# 2、地址信息细粒度特征
 # certId
 df['certId_first2'] = df['certId'].apply(lambda x: int(str(x)[:2]))  # 前两位
 df['certId_middle2'] = df['certId'].apply(lambda x: int(str(x)[2:4]))  # 中间两位
 df['certId_last2'] = df['certId'].apply(lambda x: int(str(x)[4:6]))  # 最后两位
 
-# certId 贷款
+# certId
 certId_first2_loanProduct = ['certId_first2', 'loanProduct']
 df = create_group_fea(df, certId_first2_loanProduct, 'certId_first2_loanProduct')
 certId_middle2_loanProduct = ['certId_middle2', 'loanProduct']
@@ -141,7 +150,8 @@ df = create_group_fea(df, certId_middle2_loanProduct, 'certId_middle2_loanProduc
 certId_last2_loanProduct = ['certId_last2', 'loanProduct']
 df = create_group_fea(df, certId_last2_loanProduct, 'certId_last2_loanProduct')
 
-df['certValidBegin_bin'] = pd.qcut(df['certValidBegin'], 20, labels=[i for i in range(20)])
+df['certValidBegin_bin'] = pd.qcut(df['certValidBegin'], 20, labels=[i for i in range(20)]) # 省份证有效期
+
 certId_first2_cvb = ['certId_first2', 'certValidBegin_bin']
 df = create_group_fea(df, certId_first2_cvb, 'certId_first2_cvb')
 certId_middle2_cvb = ['certId_middle2', 'certValidBegin_bin']
@@ -149,6 +159,7 @@ df = create_group_fea(df, certId_middle2_cvb, 'certId_middle2_cvb')
 certId_last2_cvb = ['certId_last2', 'certValidBegin_bin']
 df = create_group_fea(df, certId_last2_cvb, 'certId_last2_cvb')
 
+# 统计地区贷款用户评级
 certId_first2_basicLevel = ['certId_first2', 'basicLevel']
 df = create_group_fea(df, certId_first2_basicLevel, 'certId_first2_basicLevel')
 certId_middle2_basicLevel = ['certId_middle2', 'basicLevel']
@@ -156,6 +167,7 @@ df = create_group_fea(df, certId_middle2_basicLevel, 'certId_middle2_basicLevel'
 certId_last2_basicLevel = ['certId_last2', 'basicLevel']
 df = create_group_fea(df, certId_last2_basicLevel, 'certId_last2_basicLevel')
 
+# 统计地区贷款用户教育水平
 certId_first2_edu = ['certId_first2', 'edu']
 df = create_group_fea(df, certId_first2_edu, 'certId_first2_edu')
 certId_middle2_edu = ['certId_middle2', 'edu']
@@ -176,7 +188,7 @@ df['dist_first2'] = df['dist'].apply(lambda x: int(str(x)[:2]))  # 前两位
 df['dist_middle2'] = df['dist'].apply(lambda x: int(str(x)[2:4]))  # 中间两位
 df['dist_last2'] = df['dist'].apply(lambda x: int(str(x)[4:6]))  # 最后两位
 
-dist_first2_loanProduct = ['dist_first2', 'loanProduct']
+dist_first2_loanProduct = ['dist_first2', 'loanProduct'] # loanProduct
 df = create_group_fea(df, dist_first2_loanProduct, 'dist_first2_loanProduct')
 dist_middle2_loanProduct = ['dist_middle2', 'loanProduct']
 df = create_group_fea(df, dist_middle2_loanProduct, 'dist_middle2_loanProduct')
@@ -223,7 +235,7 @@ df = create_group_fea(df, residentAddr_middle2_edu, 'residentAddr_middle2_edu')
 residentAddr_last2_edu = ['residentAddr_last2', 'edu']
 df = create_group_fea(df, residentAddr_last2_edu, 'residentAddr_last2_edu')
 
-# bankCard
+# 3、对不同银行构造特征 bankCard
 df['bankCard'] = df['bankCard'].astype(int)
 df['bankCard_first6'] = df['bankCard'].apply(lambda x: int(str(x)[:6]) if x != -999 else -999)
 df['bankCard_last3'] = df['bankCard'].apply(lambda x: int(str(x)[6:].strip()) if x != -999 else -999)
@@ -247,17 +259,15 @@ df = create_group_fea(df, bankCard_last3_edu, 'bankCard_last3_edu')
 df['certValidPeriod'] = df['certValidStop'] - df['certValidBegin']
 # 类别特征处理
 
-# 特殊处理
-# bankCard 5991
-# residentAddr 5288
-# certId 4033
-# dist 3738
+
+# 4、统计count特征
+# 'bankCard', 'residentAddr', 'certId', 'dist' 稀疏类别特征->转换为count
 cols = ['bankCard', 'residentAddr', 'certId', 'dist']
 # 计数
 for col in cols:
     df['{}_count'.format(col)] = df.groupby(col)['id'].transform('count')
 
-# 对lmt进行mean encoding
+# 5、对重要特征lmt进行mean encoding
 for fea in tqdm(cols):
     grouped_df = df.groupby(fea).agg({'lmt': ['mean', 'median']})
     grouped_df.columns = [fea + '_' + '_'.join(col).strip() for col in grouped_df.columns.values]
@@ -308,6 +318,39 @@ for fea in tqdm(['bankCard_first6', 'bankCard_last3']):
     grouped_df.columns = [fea + '_' + '_'.join(col).strip() for col in grouped_df.columns.values]
     grouped_df = grouped_df.reset_index()
     df = pd.merge(df, grouped_df, on=fea, how='left')
+## 6、target 转化率特征
+## 提升分数帮助很大
+
+def get_cvr_fea(data, cat_list=None):
+    """
+    :param data:
+    :param cat_list: 类比特征
+    :return:
+    """
+    print("cat_list", cat_list)
+    # 类别特征五折转化率特征
+    print("转化率特征....")
+    data['ID'] = data.index
+    data['fold'] = data['ID'] % 5
+    # 对于训练集 fold：0，1，2，3，4
+    data.loc[data.target.isnull(), 'fold'] = 5 # 测试集
+
+    # 教育水平
+    # 研究生毕业：1  -> 0.03
+    # 中学毕业   1->  0.1
+    target_feat = []
+    for i in tqdm(cat_list):
+        target_feat.extend([i + '_mean_last_1'])
+        data[i + '_mean_last_1'] = None
+        for fold in range(6):
+            data.loc[data['fold'] == fold, i + '_mean_last_1'] = data[data['fold'] == fold][i].map(
+                data[(data['fold'] != fold) & (data['fold'] != 5)].groupby(i)['target'].mean()
+            )
+        data[i + '_mean_last_1'] = data[i + '_mean_last_1'].astype(float)
+
+    return data
+
+df=get_cvr_fea(df,cat_list=cols)
 
 # dummies
 df = pd.get_dummies(df, columns=categorical_features)
